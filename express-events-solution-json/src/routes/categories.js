ee9cdb7e7df5f57d01411453786d62786e4742a2
Kristof Siket <kristofsiket@kinsta.com>
2023-08-10 09:02:24 +0200

    feat: category CRUD routes finished

--- express-events-solution-json/src/routes/categories.js
+++ express-events-solution-json/src/routes/categories.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import getCategories from "../services/categories/getCategories.js";
 import createCategory from "../services/categories/createCategory.js";
+import getCategoryById from "../services/categories/getCategoryById.js";
+import deleteCategoryById from "../services/categories/deleteCategoryById.js";
+import updateCategoryById from "../services/categories/updateCategoryById.js";
 
 const router = Router();
 
@@ -14,6 +17,50 @@ router.post("/", (req, res) => {
   const newCategory = createCategory(name);
 
   res.status(201).json(newCategory);
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const category = getCategoryById(id);
+
+  if (!category) {
+    res.status(404).json({ message: `Category with id ${id} not found` });
+  } else {
+    res.status(200).json(category);
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const category = deleteCategoryById(id);
+
+  if (category) {
+    res.status(200).send({
+      message: `Category with id ${id} successfully deleted`,
+      category,
+    });
+  } else {
+    res.status(404).json({
+      message: `Category with id ${id} not found`,
+    });
+  }
+});
+
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const category = updateCategoryById(id, { name });
+
+  if (category) {
+    res.status(200).send({
+      message: `Category with id ${id} successfully updated`,
+      category,
+    });
+  } else {
+    res.status(404).json({
+      message: `Category with id ${id} not found`,
+    });
+  }
 });
 
 export default router;
