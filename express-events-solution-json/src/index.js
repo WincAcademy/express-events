d13cf42359f3be9ce0df9bd2a120d54fd31597cf
Kristof Siket <kristofsiket@kinsta.com>
2023-08-12 08:37:55 +0200

    feat: auth route config

--- express-events-solution-json/src/index.js
+++ express-events-solution-json/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import usersRouter from "./routes/users.js";
 import eventsRouter from "./routes/events.js";
 import categoriesRouter from "./routes/categories.js";
+import loginRouter from "./routes/login.js";
 
 const app = express();
 app.use(express.json());
@@ -10,6 +11,9 @@ app.use(express.json());
 app.use("/users", usersRouter);
 app.use("/events", eventsRouter);
 app.use("/categories", categoriesRouter);
+
+// Login
+app.use("/login", loginRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
